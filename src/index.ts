12b5aa7f2198409332e2ef04af2382e23e36e79b
Karen Lettieri <karulettieri2109@gmail.com>
2024-09-24 17:02:14 -0300

    adding express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import express from "express";
 import { TelegramController } from "./infrastructure/controllers/Telegram.controller";
 
 dotenv.config();
@@ -7,3 +8,12 @@ const telegramController = new TelegramController();
 telegramController.start();
 
 console.info("Bot de Telegram iniciado...");
+
+const server = express();
+server.use(express.json());
+
+server.get("/health", (req, res) => {
+  res.send("OK :)");
+});
+
+server.listen(80);
